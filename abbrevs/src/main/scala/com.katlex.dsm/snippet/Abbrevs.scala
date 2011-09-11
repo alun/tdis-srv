235b4572079fc1d6e257cc31baf2c9c353ac25e6
Alexey Lunacharsky <alun@katlex.com>
2011-09-11 23:46:33 +0700

    Rewritten to be more lifty

--- abbrevs/src/main/scala/com.katlex.dsm/snippet/Abbrevs.scala
+++ abbrevs/src/main/scala/com.katlex.dsm/snippet/Abbrevs.scala
@@ -1,28 +1,30 @@
 package com.katlex.dsm.snippet
 
-import xml.NodeSeq
 import com.katlex.dsm.abbrevs.TDISSiteAbbrevs
+import xml.NodeSeq
+import net.liftweb._
+import http.js.jquery.JqJsCmds.JqSetHtml
+import http.{SessionVar, SHtml}
+import util.Helpers._
 
 class Abbrevs {
+  val id = "abbrebsContainer"
+  object filterString extends SessionVar[String]("")
 
-  def table(seq : NodeSeq) = <div class="abbrevs">{
-      TDISSiteAbbrevs.abbrs.map { kv =>
-        <div class="abbrev" abbr={kv._2}>{kv._1}</div>
+  def table = ".abbrevs [id]" #> id & ".abbrevs *" #> {
+    ".abbrev" #> TDISSiteAbbrevs.abbrs.filter { kv =>
+        kv._1.toLowerCase.indexOf(filterString.get) != -1 ||
+        kv._2.toLowerCase.indexOf(filterString.get) != -1
+      } .map { kv =>
+        "* *" #> kv._1 &
+        "* [abbr]" #> kv._2
       } .toSeq
-    }</div><div id="abbrevMeaning"></div><script type="text/javascript"><![CDATA[
-      $("#abbrevMeaning").css({
-        position: "absolute"
-      }).hide();
+  }
 
-      $(".abbrev").mousemove(function (e) {
-        // console.log($(this).attr("abbr"), e.pageX, e.pageY);
-        $("#abbrevMeaning").text($(this).attr("abbr")).css({
-          left: e.pageX + 10 + "px",
-          top: e.pageY + 10 + "px"
-        }).show();
-      });
-      $(".abbrev").mouseout(function () {
-        $("#abbrevMeaning").hide();
-      });
-    ]]></script>
+  def test(in : NodeSeq) = table(<span class="lift:embed?what=_abbrevs"/>)
+
+  def search = ".input" #> SHtml.ajaxText(filterString.get, { input =>
+    filterString.set(input.toLowerCase)
+    JqSetHtml(id, <span class="lift:embed?what=_abbrevs"/>)
+  }, "id" -> "quickSearch")
 }
